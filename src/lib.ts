6c2be8a1529cd96982b19d77c40c416166ba457c
Tascord <imflo.pink@gmail.com>
2022-11-06 01:25:42 +1100

    NPM Ignore

---
+++ src/lib.ts
@@ -0,0 +1,28 @@
+import useTheme from "./hooks/Theme";
+import FlProvider from "./providers/FlProvider";
+
+import Avatar from "./components/Avatar";
+import Blockquote from "./components/Blockquote";
+import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
+import Divider from "./components/Divider";
+import Input from "./components/Input";
+import Radio from "./components/Radio";
+import Slider from "./components/Slider";
+import Stack from "./components/Stack";
+
+export default {
+    FlProvider,
+    useTheme,
+
+    Avatar,
+    Blockquote,
+    Button,
+    Checkbox,
+    Divider,
+    Input,
+    Radio,
+    Slider,
+    Stack,
+    Text
+}
